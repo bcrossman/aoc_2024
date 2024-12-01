3b509cf38966ea8641c9116adf69a786ff9b0941
Brent Crossman <brent.crossman@gmail.com>
2024-12-01 06:43:54 -0500

    day1

---
+++ day_1.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+file <- readLines("./inputs/input1.txt")
+
+locations <- (lapply(file, function(x) as.numeric(unlist(strsplit(x, "   ")))))
+
+## Part 1
+df <- 
+file %>% 
+  tibble() %>% 
+  separate(col = ".", into = c("v1", "v2"), sep = "   ", convert = T) 
+
+df %>% 
+  mutate(v1 = sort(v1),
+         v2 = sort(v2),
+         dif = abs(v1-v2)) %>%
+  pull(dif) %>% 
+  sum()
+  
+## Part 1
+count_map <- 
+  df %>% 
+  count(v2)
+
+df %>% 
+  select(v1) %>% 
+  left_join(count_map, by = c("v1"="v2")) %>% 
+  mutate(id = v1*n) %>% 
+  pull(id) %>% 
+  sum(na.rm = T)
+
+
